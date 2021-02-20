024d054f7f6617d9d208a4f7e8f4e30e4ea6680c
Kevin Agoh <kevinagoh@hotmail.fr>
2021-02-20 23:31:04 +0100

    review model created

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,8 +1,8 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_many :reviews, dependent: :destroy
 
   validates :name, presence: true
   validates :name, uniqueness: true
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_110558) do
+ActiveRecord::Schema.define(version: 2021_02_20_222616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2021_02_18_110558) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
